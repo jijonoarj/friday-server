c969bff591243071ef145b75966557fca05f0017
jijonoarj <asd15as1992@gmail.com>
2025-04-21 13:28:05 +0800

    Add files via upload
    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "👋 Hello from Friday's brain."
+
+@app.route("/set_alarm", methods=["GET"])
+def set_alarm():
+    time = request.args.get("time", "06:45")
+    print(f"[Friday] Alarm requested for: {time}")
+    return f"Alarm set for {time} (simulated)", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
