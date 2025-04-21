5eb47ffdd8d0ee23dcd8707421603cc02b78c512
jijonoarj <asd15as1992@gmail.com>
2025-04-21 16:28:46 +0800

    app.py 업데이트

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-import os
-import uuid
-import logging
 import requests
+import time
 from flask import Flask, request
-
-# Render 로그 출력을 위한 설정
-logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
@@ -16,12 +11,11 @@ def home():
 @app.route("/set_alarm", methods=["GET"])
 def set_alarm():
     time_str = request.args.get("time", "06:45")
-    unique_id = uuid.uuid4()  # 고유한 ID 생성
-    full_text = f"set_alarm:=:{time_str}:{unique_id}"  # Join에서 중복 방지
+    timestamp = int(time.time())  # 중복 방지용
+    full_text = f"set_alarm:=:{time_str}:{timestamp}"
 
-    logging.info(f"[Friday] Alarm requested for: {time_str} (msg: {full_text})")
+    print(f"[Friday] Alarm requested for: {time_str} (msg: {full_text})")
 
-    # Join API 호출
     requests.get("https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush", params={
         "apikey": "b42fb597f8604f96b8fb46375cd79b5d",
         "deviceId": "b42fb597f8604f96b8fb46375cd79b5d",
@@ -35,5 +29,4 @@ def ping():
     return "pong", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render가 지정한 포트를 사용
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
